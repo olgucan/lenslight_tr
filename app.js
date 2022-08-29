25a673892b7c4428d0dd55f00310f2a17b8ed264
olgucan <olgucan@4alabs.io>
2022-08-29 18:04:21 +0300

    lesson 1

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+
+const app =express()
+
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.send('index sayfası 2')
+})
+
+app.listen(port,()=>{
+     console.log(`Application running on Port ${port}`)
+})
